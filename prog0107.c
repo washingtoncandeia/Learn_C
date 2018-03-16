e772d1822ce0cf013c37418a55735485a241874c
Washington Candeia <araujowash@gmail.com>
2018-03-16 16:13:07 -0300

    C

---
+++ prog0107.c
@@ -0,0 +1,13 @@
+/*
+Livro: Linguagem C
+Autor: Luís Damas
+*/
+#include <stdio.h>
+
+// Escrever um programa com sinal sonoro
+int main(void){
+    printf("Aqui vai um Apito\a\n");
+    printf("Aqui vai um apito\7\n");
+
+    return 0;
+}
