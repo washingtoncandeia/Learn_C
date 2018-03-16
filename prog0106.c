552d0ae20a6c19e7497d9c944e2b294ea41a231c
Washington Candeia <araujowash@gmail.com>
2018-03-16 16:12:38 -0300

    C

---
+++ prog0106.c
@@ -0,0 +1,16 @@
+/*
+Livro: Linguagem C
+Autor: Luís Damas
+*/
+
+#include <stdio.h>
+
+// Programa com saida conforme descrito, p.16
+int main(void){
+    printf("1 -\tClientes\n");
+    printf("2 -\tFornecedores\n");
+    printf("3 -\tFaturas\n\n");
+    printf("4 -\tSair\n");
+
+    return 0;
+}
